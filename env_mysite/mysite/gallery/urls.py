88eda2ec44dc75f190892a325639d8f56e88d2d6
Amy Sitwala <asitwala@Amys-MacBook-Pro.local>
2016-10-18 02:31:12 -0700

    Fixed detail on the individual gallery pages

--- env_mysite/mysite/gallery/urls.py
+++ env_mysite/mysite/gallery/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
 	url(r'^gallery/$', views.gallery_page, name = 'gallery_page'),
+	url(r'^gallery/(?P<pk>\d+)/$', views.gallery_detail, name = "gallery_detail"),
 ]
--- env_mysite/mysite/gallery/views.py
+++ env_mysite/mysite/gallery/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Painting
 
 def gallery_page(request): 
-	paintings = Painting.objects.filter(published_date__lte = timezone.now()).order_by('published_date')
+	paintings = Painting.objects.all()[::-1] # place newer objects at top
 	return render(request, 'gallery/gallery_page.html', {'paintings': paintings})
+
+# reference: https://tutorial.djangogirls.org/en/extend_your_application/
+def gallery_detail(request, pk):
+	painting = get_object_or_404(Painting, pk=pk)
+	return render(request, 'gallery/gallery_detail.html', {'painting': painting}) 
+
