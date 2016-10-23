1d0ca186e1040c88993666102d03657ea71eabd6
Amy Sitwala <asitwala@Amys-MacBook-Pro.local>
2016-10-23 03:46:55 -0700

    Setting debug = False in settings

--- env_mysite/mysite/mysite/settings.py
+++ env_mysite/mysite/mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'mez!cm$(*d!34v(7anz(wludp=2@qrip(xm_se$v(0flko!*_-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['http://asitwala.herokuapp.com/']
 
